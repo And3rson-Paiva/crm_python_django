c1159f9840ad5de0a8e21a8f706d1c6bae6e7302
And3rson-Paiva <and3rsonpaiva@gmail.com>
2020-10-15 15:56:33 -0300

    Criado classe de visualização no admin e registro de site

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente
 
-# Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'primeiro_nome', 'ultimo_nome', 'email']
+
+
+
+admin.site.register(Cliente)
